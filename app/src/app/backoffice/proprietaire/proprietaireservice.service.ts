31f2da9cfb9d984995fb09cb6b4abe050aff39e6
marwa <maroua_touhami@yahoo.com>
2020-10-01 10:32:28 +0100

    alert success

--- app/src/app/backoffice/proprietaire/proprietaireservice.service.ts
+++ app/src/app/backoffice/proprietaire/proprietaireservice.service.ts
@@ -1,21 +1,21 @@
 import { FormGroup, FormControl } from '@angular/forms';
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ProprietaireserviceService {
 
-  constructor() { }
-  form: FormGroup = new FormGroup({
-    $key: new FormControl(null),
-    fullName: new FormControl(''),
-    email: new FormControl(''),
-    mobile: new FormControl(''),
-    city: new FormControl(''),
-    gender: new FormControl(''),
-    department : new FormControl(0),
-    hireDate : new FormControl(''),
-    isPermanent : new FormControl(false),
-  })
+  constructor(private http: HttpClient) { }
+  // form: FormGroup = new FormGroup({
+  //   $key: new FormControl(null),
+  //   fullName: new FormControl(''),
+  //   email: new FormControl(''),
+  //   mobile: new FormControl(''),
+  //   city: new FormControl(''),
+  //   gender: new FormControl(''),
+  //   department : new FormControl(0),
+  //   hireDate : new FormControl(''),
+  //   isPermanent : new FormControl(false),
+  // })
 }
